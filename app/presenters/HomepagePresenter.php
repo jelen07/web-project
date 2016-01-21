5b408b6d7d5000edb233b2bd32465d14426f3d30
Vojtech Lacina <MoraviaD1@gmail.com>
2016-01-21 12:53:49 +0100

    No App template

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -7,4 +7,16 @@ use Nette;
 
 class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+
+	public function startup()
+	{
+		$dev = isset($_COOKIE['debug_please']) && $_COOKIE['debug_please'] == 'idontgiveabuck07';
+		// $cli = php_sapi_name() == 'cli';
+
+		if (!$dev) {
+			$this->setView('noapp');
+		}
+
+		parent::startup();
+	}
 }
